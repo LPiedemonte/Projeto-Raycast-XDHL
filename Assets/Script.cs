18ee215cc29283a6b0936b94ce1e854d8be82789
LPiedemonte <laura.vargas.pied@gmail.com>
2026-06-12 20:13:10 -0300

    Alguns cods arrumados

--- Assets/Script.cs
+++ Assets/Script.cs
@@ -3,61 +3,42 @@ using UnityEngine;
 
 public class Script : MonoBehaviour
 {
-    /*Ray ray;
+    Ray ray;
     RaycastHit hitData;
     Vector3 point;
-    Color color;*/
-    public Camera _camera;
+    Color color;
+    public LayerMask hitMask;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
-        {
-            if (hit.transform.gameObject.CompareTag("Alvo"))
-            {
-                
-            }
-            Debug.DrawRay(transform.position, transform.forward * 9, Color.purple);
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.forward * 9, Color.red);
+        if(Input.GetMouseButton(0)){
+            Targetray();
         }
     }
-    /*private void Lancar(Ray ray, Color color, int tipo)
-    {
-        Debug.Log("Origem: " + ray.origin);
 
-        Debug.Log("Direção: " + ray.direction);
+    void Targetray(){
+        Ray ray = amera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hitData))
-        {
-            Vector3 hitPosition = hitData.point;
-            Debug.Log("hitPosition:" + hitPosition);
-
-            float hitDistance = hitData.distance;
-            Debug.Log("Distancia:" + hitDistance);
-            string tag = hitData.collider.tag;
-            Debug.Log("tag:" + tag);
-            GameObject hitObject = hitData.transform.gameObject;
-            Debug.DrawRay(ray.origin, hitPosition * hitDistance, Color.purple);
-            
-            if (tag =="Alvo")
-                Destroy(hitObject);
+        if(Physics.Raycast(transform.position, transform.foward, out hit)){
+            if(hit.transform.gameObject.CompareTag("Alvo")){
+                Debug.DrawRay(transform.position, transform.foward * 9, Color.yellow, 0.5f);
+                Debug.Log(hit.transform.gameObject.name);
+            }
+            else{
+                Debug.DrawRay(transform.position, transform.foward * 9, Color.blue, 0.5f);
+                Debug.Log(hit.transform.gameObject.name);
+            }
         }
-        else
-        {
-            Debug.DrawRay(ray.origin, ray.direction * 1000, Color.white);
-            Debug.Log("errou alvo");
+        else{
+            Debug.DrawRay(transform.position, transform.foward * 9, Color.purple, 0.5f);
         }
-    }*/
+    }
+    
 }
    
